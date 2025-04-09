6a27f65c70637e56bc107625e38e609848d3b6fb
Amanat <amanat.ali@ginkgoretail.com>
2025-04-09 11:26:55 +0500

    updation

--- ChatModels/chatmodel_openai.py
+++ ChatModels/chatmodel_openai.py
@@ -5,4 +5,4 @@ load_dotenv()
 chat_model = ChatOpenAI(model="gpt-3.5-turbo-instant")
 
 result = chat_model.invoke("Who is the chief of army staff of Pakistan?")
-print(result.content) #It is not a string output
+print(result.content)
